556318b15a6f98e9ffce63bcd76ca2f44a270d57
waheed khan <wahid.khan74@gmail.com>
2020-05-20 05:23:35 -0500

    string types

---
+++ JavaScript/string-type.js
@@ -0,0 +1,46 @@
+let msg  = 'Today is "good day" !';
+console.log(msg);
+typeof msg ;
+
+
+let msg1 = "Tommorow is ParleG ";
+typeof msg1;
+
+let msg2 = 'Yesterday Marry Gold';
+typeof msg2;
+
+let msg3 = `Multi 
+line 
+string`;
+msg3;
+
+//multi line string  // expression string.
+
+let  name  = 'John';
+let age  = 29;
+
+let msg4 = 'Hi, ' + name + ' Your age is, ' +age;
+msg4;
+
+let msg5 = `Hi, ${name} Your age is, ${age}`;
+
+let x = 10;
+let y = 20;
+
+let z = ` Total value of x and y is : ${x+y+20-20-0+20}`;
+
+msg5;
+z;
+
+
+
+
+
+
+
+
+
+
+
+
+
